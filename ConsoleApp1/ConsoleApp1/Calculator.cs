4900e7fed2e78f7e914d825dcd79a2d1849d6b77
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-15 01:03:47 +0300

    Тесты 88,24%

---
+++ ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Calculator
+    {
+        public static decimal Calc(decimal arg1, decimal arg2, string @oper)
+        {
+            var result = @oper switch
+            {
+                "+" => arg1 + arg2,
+                "-" => arg1 - arg2,
+                "*" => arg1 * arg2,
+                "/" => arg1 / arg2,
+                _ => throw new NotSupportedException()
+            };
+            return result;
+        }
+        public static decimal GetNumber(string s)
+        { 
+            return decimal.Parse(s);
+        }
+
+        public static string[] Split(string s)
+        {
+            return s.Split();
+        }
+        public static decimal OutPut(string s)
+        {
+            var str = Split(s);
+            var arg1 = GetNumber(str[0]);
+            var arg2 = GetNumber(str[2]);
+            return Calc(arg1, arg2, str[1]);
+        }
+    }
+}
--- ConsoleApp1/NUnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/NUnitTestProject1/UnitTest1.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using ConsoleApp24;
+using ConsoleApp1;
 namespace NUnitTestProject1
 {
     public class Tests
@@ -8,6 +8,7 @@ namespace NUnitTestProject1
         public void Setup()
         {
         }
+
         [Test]
         public void Multiply() => Assert.AreEqual(4, Calculator.Calc(2, 2, "*"));
         [Test]
@@ -16,5 +17,15 @@ namespace NUnitTestProject1
         public void Plus() => Assert.AreEqual(4, Calculator.Calc(2, 2, "+"));
         [Test]
         public void Minus() => Assert.AreEqual(0, Calculator.Calc(2, 2, "-"));
+        [Test]
+        public void GetNumber() => Assert.AreEqual(1.0, Calculator.GetNumber("1,0"));
+        [Test]
+        public void OutPut() => Assert.AreEqual(8, Calculator.OutPut("4 * 2"));
+        [Test]
+        public void Split0() => Assert.AreEqual("1", Calculator.Split("1 / 2")[0]);
+        [Test]
+        public void Split1() => Assert.AreEqual("/", Calculator.Split("1 / 2")[1]);
+        [Test]
+        public void Split2() => Assert.AreEqual("2", Calculator.Split("1 / 2")[2]);
     }
 }
