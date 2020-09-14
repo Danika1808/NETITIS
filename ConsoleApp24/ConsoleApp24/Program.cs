47a88af3e5db305e618a9051fa9c3b1a06a2b1af
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-14 19:42:28 +0300

    var перменные

---
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split();
+            var arg1 = Calculator.GetNumber(s[0]);
+            var arg2 = Calculator.GetNumber(s[2]);
+            Console.WriteLine(Calculator.Calc(arg1, arg2, s[1]));
+        }
+    }
+}
