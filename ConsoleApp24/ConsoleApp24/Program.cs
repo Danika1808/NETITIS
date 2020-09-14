98b1b7e1194eb08d3ecea35c5bdcadc09a67ce72
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-14 18:44:58 +0300

    упростил код

--- ConsoleApp24/ConsoleApp24/Program.cs
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp24
 {
     class Program
     {
-        static void Main()
-        {
+         static void Main()
+         {
             var s = Console.ReadLine().Split();
-            double arg1 = Сalculator.GetNumber(s[0]);
-            double arg2 = Сalculator.GetNumber(s[2]);
+            var arg1 = Сalculator.GetNumber(s[0]);
+            var arg2 = Сalculator.GetNumber(s[2]);
             Console.WriteLine(Сalculator.Calc(arg1, arg2, s[1]));
         }
     }
