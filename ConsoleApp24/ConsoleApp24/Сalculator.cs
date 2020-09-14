d0b1044b621b1adbbf5e8fa5225c69b752d22170
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-14 18:46:03 +0300

    Сделал обработку исключений и сделал тип данных decimal

--- ConsoleApp24/ConsoleApp24/Сalculator.cs
+++ ConsoleApp24/ConsoleApp24/Сalculator.cs
@@ -1,29 +1,42 @@
 ﻿using System;
-using System.Diagnostics.Eventing.Reader;
 
 
 namespace ConsoleApp24
 {
     public static class Сalculator
     {
-        public static double Calc(double arg1, double arg2, string oper)
+        public static decimal Calc(decimal arg1, decimal arg2, string oper)
         {
             switch (oper)
             {
                 case "+": return arg1 + arg2;
                 case "-": return arg1 - arg2;
                 case "*": return arg1 * arg2;
-                case "/": return arg1 / arg2;
+                case "/":
+                    try
+                    {
+                        return arg1 / arg2;
+                    }
+                    catch (DivideByZeroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
                 default:
                     throw new ArgumentException($"Неверная операция {oper}");
             }
         }
-        public static double GetNumber(string str)
+        public static decimal GetNumber(string str)
         {
-            if (double.TryParse(str, out _))
-                return double.Parse(str);
-            else
-                throw new ArgumentException($"Неверное значение");
+            try
+            {
+                return decimal.Parse(str);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }
