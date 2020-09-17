d3208a4a4d93155c00eeb3b7c5e5e3bbdfa7fe6d
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-17 18:36:15 +0300

    Update UnitTest1.cs

--- ConsoleApp1/NUnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/NUnitTestProject1/UnitTest1.cs
@@ -11,11 +11,11 @@ namespace NUnitTestProject1
         [Test]
         public void Plus() => Assert.AreEqual(4, Calculator.Calc(2, 2, "+"));
         [Test]
+        public void Minus() => Assert.AreEqual(0, Calculator.Calc(2, 2, "-"));
+        [Test]
         public void Multiply() => Assert.AreEqual(4, Calculator.Calc(2, 2, "*"));
         [Test]
         public void Devide() => Assert.AreEqual(1, Calculator.Calc(2, 2, "/"));
-        [Test]
-        public void Minus() => Assert.AreEqual(0, Calculator.Calc(2, 2, "-"));
         [Test]
         public void GetNumber() => Assert.AreEqual(1.0, Calculator.GetNumber("1,0"));
         [Test]
