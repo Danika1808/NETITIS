949b22fb2981825d6a2198f8c433d8296e20c3a9
Danika1808 <Danika1808@users.noreply.github.com>
2020-09-18 00:05:23 +0300

    Plz god

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = Calculator.Calc(2, 2, "*");
+            Console.WriteLine(result);
+        }
+    }
+}
