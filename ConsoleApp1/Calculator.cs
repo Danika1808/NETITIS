03b840ed2f1e552fe900adec7850d554b1b45633
Danika1808 <65758418+Danika1808@users.noreply.github.com>
2020-09-17 23:29:43 +0300

    Update Calculator.cs

--- ConsoleApp1/Calculator.cs
+++ ConsoleApp1/Calculator.cs
@@ -1,43 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleApp1
 {
-    public static class Сalculator
-    {
-        public static void Calc(decimal arg1, decimal arg2, string @oper, out decimal result)
-        {
-            var temp = @oper switch
-            {
-                "+" => arg1 + arg2,
-                "-" => arg1 - arg2,
-                "*" => arg1 * arg2,
-                "/" => arg1 / arg2,
-                _ => throw new NotSupportedException()
-            };
-            result = temp;
-        }
-        public static decimal GetNumber(string s)
-        {
-            try
-            {
-                return decimal.Parse(s);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+public static class Calculator
+{
+public static decimal Calc(decimal arg1, decimal arg2, string @oper)
+{
+var result = @oper switch
+{
+"+" => arg1 + arg2,
+"-" => arg1 - arg2,
+"*" => arg1 * arg2,
+"/" => arg1 / arg2,
+_ => throw new NotSupportedException()
+};
+return result;
+}
+public static decimal GetNumber(string s)
+{
+return decimal.Parse(s);
+}
 
-        public static decimal Input(string s)
-        {
-            string[] str = s.Split();
-            var arg1 = GetNumber(str[0]);
-            var arg2 = GetNumber(str[2]);
-            Calc(arg1, arg2, str[1], out decimal result);
-            return result;
-        }
-    }
+public static string[] Split(string s)
+{
+return s.Split();
+}
+public static decimal OutPut(string s)
+{
+var str = Split(s);
+var arg1 = GetNumber(str[0]);
+var arg2 = GetNumber(str[2]);
+return Calc(arg1, arg2, str[1]);
+}
+}
+}
+ 
+using System;
+
+namespace ConsoleApp1
+{
+class Program
+{
+static void Main()
+{
+Console.WriteLine(Calculator.OutPut(Console.ReadLine()));
+}
+}
 }
