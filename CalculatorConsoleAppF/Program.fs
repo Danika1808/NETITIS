6688494398f006615e356a7e99ca126c3b345d08
Danil <danikavsilev@gmail.com>
2020-09-28 16:07:46 +0300

    Калькулятор на F# вроде работает

---
+++ CalculatorConsoleAppF/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let add x y = x + y
+
+let subtract x y = x - y
+
+let divide x y = x / y
+
+let multiply x y = x * y
+
+let calculator x y = match x y with
+| "+" -> add x y
+| "-" -> subtract x y
+| "/" -> divide x y
+| "*" -> multiply x y
+| _ -> Exception
+[<EntryPoint>]
