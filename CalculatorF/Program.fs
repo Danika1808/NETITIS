245a496f2b4fcd4d80c6932ffbff583867f06fd0
Danil <danikavsilev@gmail.com>
2020-09-29 03:28:05 +0300

    Убрал костыли, теперь все работает без свистоперделок

--- CalculatorF/Program.fs
+++ CalculatorF/Program.fs
@@ -1,5 +1,7 @@
 ﻿open System
 open System.Diagnostics.CodeAnalysis
+open System.Globalization
+open System.Net
 
 type MaybeBuilder() =
     member this.Bind(x, f) = 
@@ -26,16 +28,33 @@ let divide x y =
 
 let calculate op x y =
         match op with
-            | "+" -> Some (add x y)
-            | "-" -> Some (subtract x y)
-            | "*" -> Some (multiply x y)
+            | "+" -> Some(add x y)
+            | "-" -> Some(subtract x y)
+            | "*" -> Some(multiply x y)
             | "/" -> divide x y
+            | _ -> None
 
+let styles = NumberStyles.AllowDecimalPoint
+let provider = new CultureInfo("en-US")
+
+let getNumber (str:string) = 
+         let isNumber, number = Double.TryParse(str, styles, provider)
+         if isNumber then Some(number) else None
+
+let output  (result : float option)  =
+    if
+        result = None then Console.WriteLine("Введено неверное значение или попытка деления на нуль")
+    else
+        Console.WriteLine(result.Value)
+        
 [<EntryPoint>]
-let main argv=           
-    let a : float  = Console.ReadLine() |> Double.Parse
+let main argv = 
+    let a = getNumber(Console.ReadLine()).Value
     let op = Console.ReadLine()
-    let b : float  = Console.ReadLine() |> Double.Parse
-    let result: float option = calculate op a b
-    Console.WriteLine(result.Value)
+    let b = getNumber(Console.ReadLine()).Value
+    let result = calculate op a b
+    output result
     0
+    
+   
+ 
