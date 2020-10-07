e3aa19dec98a4b697991fc54ab7ebcd0185c8a53
Danika1808 <65758418+Danika1808@users.noreply.github.com>
2020-10-08 00:26:03 +0300

    Maldelbrot set (#15)
    * MaldelbrotSet
    
    * Добавил maybebuilder в main и output
    
    * Все построилось все работает, тока для терпеливых
    
    * Исправил магические числа и убрал лишний код
    
    Co-authored-by: Danil <danikavsilev@gmail.com>

---
+++ Maldelbrot/Maldelbrot app/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Maldelbrot_app
+{
+    public partial class Form1 : Form
+    {
+        private double _hx, _hy;
+        private Bitmap _bmp;
+        private double _sizeArea = 3;
+        private readonly double _zoom = 1.1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Draw();
+            timer1.Start();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int X = e.X,
+                Y = e.Y;
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    _hx = Fractal.temp(X, _hx, _sizeArea, pictureBox1.Width);
+                    _hy = Fractal.temp(Y, _hy, _sizeArea, pictureBox1.Height);
+                    Draw();
+                    break;
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            _sizeArea /= _zoom;
+            Draw();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = _bmp;
+        }
+
+        private void Draw()
+        {
+            _bmp = Fractal.createImage(_sizeArea, pictureBox1.Height, pictureBox1.Width, _hx, _hy);
+            pictureBox1.Image = _bmp;
+        }
+    }
+}
---
+++ Maldelbrot/MaldelbrotSet/Fractal.fs
@@ -0,0 +1,29 @@
+﻿module Fractal
+open System.Drawing
+open System.Windows.Forms
+open System.Numerics
+open System
+let temp c f size k = f - (size / 2.0) + (float c) * size / k 
+
+let createImage sizeArea heigth wight hx hy =
+    let image = new Bitmap((int heigth), (int wight))
+    for x in 0..image.Width-1 do
+        for y in 0..image.Height-1 do
+            let mutable x_ = temp x hx sizeArea wight
+            let mutable y_ = temp y hy sizeArea heigth
+            let mutable z = new Complex(0.0, 0.0)
+            let mutable it = 0
+            let mutable Break = false
+            while (Break = false && it < 100) do
+                it <- it + 1
+                z <- z*z
+                z <- z + new Complex(x_, y_)
+                if (z.Magnitude > 2.0) then Break <- true 
+                else 
+                    image.SetPixel(x, y, Color.FromArgb(255, it % 8 * 16, it % 4 * 32, it % 2 * 64))
+    image
+
+
+
+            
+
