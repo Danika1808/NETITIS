ba3ba838e5844e965dcb4ae927afe3a5d025af47
Danil <danikavsilev@gmail.com>
2020-10-12 18:28:14 +0300

    Клиент

---
+++ CalculatorASP/ProxyF/Program.fs
@@ -0,0 +1,69 @@
+﻿open System
+open System.Net
+open System.Text.RegularExpressions;
+open System.IO
+
+type MaybeBuilder() =
+    member this.Bind(x,f)=
+        match x with
+        |None -> None
+        |Some a -> f a
+    member this.Return(x)=
+        Some x
+
+let maybe = new MaybeBuilder()
+
+type AsyncMaybeBuilder () =
+    member this.Bind(x, f) =
+        async {
+            let! _x = x
+            match _x with
+            | Some s -> return! f s
+            | None -> return None
+            }
+
+    member this.Return(x:'a option) =
+        async{return x}
+
+let asyncMaybe = new AsyncMaybeBuilder()
+
+let checkAccess input =
+    Regex.IsMatch(input, @"(-?\d+(?:\.\d+)?)\s*([-%2B*%2F])\s*(-?\d+(?:\.\d+)?)")
+
+let output  (result : float option)  =
+    match result with
+        | None -> Console.WriteLine("Mission Failed we'll get em next time")
+        | Some result -> Console.WriteLine(result)
+        
+let getOper op = 
+    match op with
+    | "+" -> "%2B"
+    | "*" -> "*"
+    | "/" -> "%2F"
+    | "-" -> "-"
+    | _ -> ""
+
+let giveRequest expression = async{
+
+    let request = WebRequest.Create("http://localhost:51963?value=" + expression, Method = "GET", ContentType = "text/plain")
+    let response  = request.GetResponse()
+    let stream = response.GetResponseStream();
+    let readStream = new StreamReader(stream)
+    return readStream.ReadToEnd()
+    }
+
+let RunCalculator expression =
+    let result = Async.RunSynchronously(giveRequest expression)
+    result
+
+[<EntryPoint>]
+let main argv = 
+    let a = Console.ReadLine()
+    let op = getOper(Console.ReadLine())
+    let b = Console.ReadLine()
+    let expression = a + " " + op + " " + b;
+    let result = match checkAccess expression with
+    | true -> RunCalculator expression
+    | false -> "Bad request"
+    printf"%s"result
+    0     
