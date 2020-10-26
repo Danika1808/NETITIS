5b8a77b49d74e5ac49a2f7c05174f5aa35b4fdde
Danil <danikavsilev@gmail.com>
2020-10-26 20:47:20 +0300

    Сделал запросы компактнее

--- CalculatorASP/ExpressionTree/Responce.cs
+++ CalculatorASP/ExpressionTree/Responce.cs
@@ -5,41 +5,17 @@ using System.Net.Http;
 namespace ExpressionTree
 {
     class Responce
-    {
-        public static string GetResponsePlus(Expression a, Expression b)
-        {
-            string a1 = a.ToString();
-            string b1 = b.ToString();
-            string oper = "%2B";
-            var ans = GetPesponsing(a1, b1, oper);
-            return ans;
-        }
-        public static string GetResponseMinus(Expression a, Expression b)
-        {
-            string a1 = a.ToString();
-            string b1 = b.ToString();
-            string oper = "-";
-            var ans = GetPesponsing(a1, b1, oper);
-            return ans;
-        }
-        public static string GetResponseDivide(Expression a, Expression b)
-        {
-            string a1 = a.ToString();
-            string b1 = b.ToString();
-            string oper = "%2F";
-            var ans = GetPesponsing(a1, b1, oper);
-            return ans;
-        }
-        public static string GetResponseMultply(Expression a, Expression b)
-        {
-            string a1 = a.ToString();
-            string b1 = b.ToString();
-            string oper = "*";
-            var ans = GetPesponsing(a1, b1, oper);
-            return ans;
-        }
+    {  
         public static string GetPesponsing(string a, string b, string oper)
         {
+            oper = oper switch
+            {
+                "+" => "%2B",
+                "-" => "-",
+                "/" => "%2F",
+                "*" => "*",
+                _ => throw new NotImplementedException()
+            };
             string expression = a + oper + b;
             HttpClient client = new HttpClient();
             var responce = client.GetAsync("http://localhost:51963?value=" + expression).Result;
