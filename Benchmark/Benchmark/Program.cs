bfab813e804be81582508dc94610e4bc23ec44e2
Danil <danikavsilev@gmail.com>
2020-12-27 11:38:21 +0300

    Бенчмарк

---
+++ Benchmark/Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EasiestTest.OnTestsStart();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Benchmark/Benchmark/TestClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BenchmarkDotNet;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    class TestClass
+    {
+        void SimulateWork()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+                builder.Append(i.ToString());
+        }
+
+        public static void PbStaticVoid()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+                builder.Append(i.ToString());
+        }
+
+        [Benchmark(Description = "PbVoid")]
+        public void PbVoid() =>
+            SimulateWork();
+
+        [Benchmark(Description = "PbVirtualVoid")]
+        public virtual void PbVirtualVoid() =>
+            SimulateWork();
+
+        public static void PbGenericStaticVoid<T>()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+                builder.Append(i.ToString());
+        }
+
+        public static void PbGenericStaticVoid() =>
+            PbGenericStaticVoid<int>();
+
+
+        [Benchmark(Description = "PbGenericVoid")]
+        public void PbGenericVoid() =>
+            SimulateWork();
+
+        [Benchmark(Description = "PbDynamic")]
+        public dynamic PbDynamic()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+                builder.Append(i.ToString());
+            return builder;
+        }
+    }
+}
